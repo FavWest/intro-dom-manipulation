dd2756caf25ea87828f3096da86d9c74d13d77b9
Tori FavWest <practiceandthenpractice@gmail.com>
2021-03-23 13:32:44 -0700

    create buttons that display dialogue, which is removed by clicking
    Co-authored-by: Seth Medeiros <Medeirosseth@gmail.com>
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
+$(document).ready(function () {
+  $("button#hello").click(function () {
+    $("ul#you-said").prepend("<li>Hello!</li>");
+    $("ul#HAL-said").prepend("<li>Greetings, Human!</li>");
+    $("ul#you-said").children("li").first().click(function () {
+      (this).remove();
+    })
+    $("ul#HAL-said").children("li").first().click(function () {
+      (this).remove();
+    })
+  });
 
+  $("button#goodbye").click(function () {
+    $("ul#you-said").prepend("<li>Goodbye!</li>");
+    $("ul#HAL-said").prepend("<li>Goodbye</li>");
+  });
+
+  $("button#stop").click(function () {
+    $("ul#you-said").prepend("<li>Stop copying me!</li>");
+    $("ul#HAL-said").prepend("<li>I can't do that, Dave.</li>");
+  });
+});
